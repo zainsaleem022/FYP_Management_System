07b345fe5a4a137b06a4573f8020dccb50f42e80
IK <ibrahim.khalil3987@gmail.com>
2024-05-04 20:59:57 +0500

    done

--- SE_Project/RegisterStudent.aspx.cs
+++ SE_Project/RegisterStudent.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
 
 public partial class RegisterStudent : System.Web.UI.Page
 {
@@ -12,30 +13,46 @@ public partial class RegisterStudent : System.Web.UI.Page
 
     }
 
-
-
     protected void Button1_Click(object sender, EventArgs e)
     {
         int group;
         if (int.TryParse(TextBox3.Text, out group))
         {
             dbhandler dbhandler = dbhandler.Instance;
-
             List<(string id, string password, string name, string email)> students = new List<(string id, string password, string name, string email)>();
 
             // Add Student 1
             string id1 = TextBox1.Text;
             string password1 = TextBox2.Text;
             string name1 = TextBox4.Text;
-            string email1 = id1 + "@abc.pk";
-            students.Add((id1, password1, name1, email1));
+
+            string validationErrorMessage = ValidateInput(id1, password1, name1);
+            if (validationErrorMessage == string.Empty)
+            {
+                string email1 = id1 + "@abc.pk";
+                students.Add((id1, password1, name1, email1));
+            }
+            else
+            {
+                Response.Write("<script>alert('" + validationErrorMessage + "');</script>");
+                return; // Stop further processing if input for Student 1 is invalid
+            }
 
             // Add Student 2
             string id2 = TextBox5.Text;
             string password2 = TextBox6.Text;
             string name2 = TextBox7.Text;
-            string email2 = id2 + "@abc.pk";
-            students.Add((id2, password2, name2, email2));
+
+            validationErrorMessage = ValidateInput(id2, password2, name2);
+            if (validationErrorMessage == string.Empty)
+            {
+                string email2 = id2 + "@abc.pk";
+                students.Add((id2, password2, name2, email2));
+            }
+            else
+            {
+                Response.Write("<script>alert('" + validationErrorMessage + "');</script>");
+            }
 
             // Add Student 3 if the textboxes are not empty
             if (!string.IsNullOrEmpty(TextBox8.Text) && !string.IsNullOrEmpty(TextBox9.Text) && !string.IsNullOrEmpty(TextBox10.Text))
@@ -43,8 +60,37 @@ public partial class RegisterStudent : System.Web.UI.Page
                 string id3 = TextBox8.Text;
                 string password3 = TextBox9.Text;
                 string name3 = TextBox10.Text;
-                string email3 = id3 + "@abc.pk";
-                students.Add((id3, password3, name3, email3));
+
+                validationErrorMessage = ValidateInput(id3, password3, name3);
+                if (validationErrorMessage == string.Empty)
+                {
+                    string email3 = id3 + "@abc.pk";
+                    students.Add((id3, password3, name3, email3));
+                }
+                else
+                {
+                    Response.Write("<script>alert('" + validationErrorMessage + "');</script>");
+                }
+            }
+
+            // Check if no student data is entered
+            if (students.Count == 0)
+            {
+                Response.Write("<script>alert('Please enter data for at least one student.');</script>");
+                return;
+            }
+
+            // Check for minimum and maximum number of members
+            int memberCount = students.Count;
+            if (memberCount < 2)
+            {
+                Response.Write("<script>alert('A group must have at least 2 members');</script>");
+                return;
+            }
+            else if (memberCount > 3)
+            {
+                Response.Write("<script>alert('A group cannot have more than 3 members');</script>");
+                return;
             }
 
             // Perform validations and registration
@@ -66,4 +112,38 @@ public partial class RegisterStudent : System.Web.UI.Page
         }
     }
 
+    string ValidateInput(string id, string password, string name)
+    {
+        // Validate ID
+        if (id.Length != 7)
+        {
+            return "Invalid ID length. ID should be exactly 7 characters.";
+        }
+        else if (!Regex.IsMatch(id, @"^[a-zA-Z0-9]+$"))
+        {
+            return "ID should contain only alphanumeric characters.";
+        }
+
+        // Validate Password
+        if (password.Length <= 7)
+        {
+            return "Invalid password length. Password should be greater than 7 characters.";
+        }
+        else if (!Regex.IsMatch(password, @"^[a-zA-Z0-9]+$"))
+        {
+            return "Password should contain only alphanumeric characters.";
+        }
+
+        // Validate Name
+        if (name.Length <= 4)
+        {
+            return "Invalid name length. Name should be greater than 4 characters.";
+        }
+        else if (!Regex.IsMatch(name, @"^[a-zA-Z ]+$"))
+        {
+            return "Name should contain only alphabetic characters and spaces.";
+        }
+
+        return string.Empty; // Input is valid
+    }
 }
