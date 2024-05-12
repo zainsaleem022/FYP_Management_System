f5700aeebe7806ee9f393a45538f15f94e8af966
Zain Saleem <zainsaleem022@gmail.com>
2024-05-12 16:31:56 +0500

    supervisor can view assignments

---
+++ SE_Project/supervisor_view_fyp_assignments.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="supervisor_view_fyp_assignments.aspx.cs" Inherits="supervisor_view_fyp_assignments" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label runat="server" Text="Current Assignments" ID="supervisor_view_assignments_form_title"></asp:Label>
+        </div>
+        <div class="wlp-whitespace-only-element-expansion">&nbsp;</div>
+        <div class="wlp-whitespace-only-element-expansion">&nbsp;</div>
+        <div class="wlp-whitespace-only-element-expansion">
+            <asp:Label runat="server" Text="" ID="supervisor_no_assignments_found_label"></asp:Label>&nbsp;</div>
+        <div class="wlp-whitespace-only-element-expansion">&nbsp;</div>
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False">
+            <Columns>
+                <asp:BoundField DataField="FYP Title" HeaderText="FYP Title" />
+                <asp:BoundField DataField="Assignment Title" HeaderText="Assignment Title" />
+                <asp:BoundField DataField="Assignment Deadline" HeaderText="Assignment Deadline" />
+                <asp:BoundField DataField="Student Name" HeaderText="Student Name" />
+            </Columns>
+        </asp:GridView>
+
+        <div class="wlp-whitespace-only-element-expansion">&nbsp;</div>
+        <div class="wlp-whitespace-only-element-expansion">&nbsp;</div>
+        <div class="wlp-whitespace-only-element-expansion">&nbsp;</div>
+        <div class="wlp-whitespace-only-element-expansion">&nbsp;</div>
+        <div class="wlp-whitespace-only-element-expansion">&nbsp;</div>
+        <div class="wlp-whitespace-only-element-expansion">&nbsp;</div>
+        <div class="wlp-whitespace-only-element-expansion">
+            <asp:Button runat="server" Text="Back" OnClick="Unnamed1_Click"></asp:Button></div>
+        <div class="wlp-whitespace-only-element-expansion">&nbsp;</div>
+        <div class="wlp-whitespace-only-element-expansion">&nbsp;</div>
+        <div class="wlp-whitespace-only-element-expansion">&nbsp;</div>
+        <div class="wlp-whitespace-only-element-expansion">&nbsp;</div>
+        <div class="wlp-whitespace-only-element-expansion">&nbsp;</div>
+        <div class="wlp-whitespace-only-element-expansion">&nbsp;</div>
+        <div class="wlp-whitespace-only-element-expansion">&nbsp;</div>
+        <div class="wlp-whitespace-only-element-expansion">&nbsp;</div>
+        <div class="wlp-whitespace-only-element-expansion">&nbsp;</div>
+        <div class="wlp-whitespace-only-element-expansion">&nbsp;</div>
+        <div class="wlp-whitespace-only-element-expansion">&nbsp;</div>
+        <div class="wlp-whitespace-only-element-expansion">&nbsp;</div>
+        <div class="wlp-whitespace-only-element-expansion">&nbsp;</div>
+    </form>
+</body>
+</html>
---
+++ SE_Project/supervisor_view_fyp_assignments.aspx.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class supervisor_view_fyp_assignments : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // Retrieve the faculty object from the session
+        Faculty_class faculty = Session["faculty_user"] as Faculty_class;
+
+        // Get the supervisor ID
+        string supervisor_id = faculty.getId();
+
+        // Create the SQL query
+        string query = @"
+        SELECT 
+            f.fyp_title AS 'FYP Title',
+            a.title AS 'Assignment Title',
+            a.deadline AS 'Assignment Deadline',
+            STRING_AGG(s.student_name, ', ') AS 'Student Names'
+        FROM 
+            fyp f
+        INNER JOIN 
+            fyp_group fg ON f.id = fg.fyp_id
+        INNER JOIN 
+            Student s ON fg.student_id = s.student_id
+        INNER JOIN 
+            fyp_supervisor fs ON f.id = fs.fyp_id
+        LEFT JOIN 
+            Assignment_Submission sub ON f.id = sub.fyp_group_id
+        LEFT JOIN 
+            Assignment a ON sub.assignment_id = a.id
+        WHERE 
+            fs.supervisor_id = @supervisor_id
+            AND sub.id IS NULL
+        GROUP BY
+            f.fyp_title,
+            a.title,
+            a.deadline";
+
+
+        // Get the dbhandler instance
+        dbhandler dbHandler = dbhandler.Instance;
+
+        try
+        {
+            // Open the database connection
+            dbHandler.connection.Open();
+
+            // Create a SqlCommand
+            SqlCommand command = new SqlCommand(query, dbHandler.connection);
+            command.Parameters.AddWithValue("@supervisor_id", supervisor_id);
+
+            // Execute the query
+            SqlDataReader reader = command.ExecuteReader();
+
+            // Check if there are rows returned
+            if (reader.HasRows)
+            {
+                // Create a DataTable to hold the results
+                DataTable dataTable = new DataTable();
+
+                // Load the DataTable with the results from the reader
+                dataTable.Load(reader);
+
+                // Bind the DataTable to the GridView
+                GridView1.DataSource = dataTable;
+                GridView1.DataBind();
+            }
+            else
+            {
+                // No rows returned, show a message or take appropriate action
+                supervisor_no_assignments_found_label.Text = "No Assingments are Currently Due";
+            }
+        }
+        catch (Exception ex)
+        {
+            // Handle exceptions
+            // For example:
+            ClientScript.RegisterStartupScript(this.GetType(), "alert", $"alert('Error Occurred: {ex.Message}');", true);
+
+        }
+        finally
+        {
+            // Close the database connection
+            dbHandler.connection.Close();
+        }
+    }
+
+
+
+    protected void Unnamed1_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("Supervisor_Interface.aspx");
+    }
+}
